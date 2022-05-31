7c53213dbe011f7bc7363af403a6c55643b05365
Fred36533653 <106580158+Fred36533653@users.noreply.github.com>
2022-05-31 18:00:28 +0900

    굿

--- __init__.py
+++
@@ -1,5 +0,0 @@
-import marbles
-import tug_of_war
-import glass_stepping_stones
-
-__all__ = ['marbles', 'tug_of_war', 'glass_stepping_stones']
--- participant/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import computer
-from . import my_own_player
-
-__all__ = ['my_own_player']
